7c29e1a5809a8dbb2b7de40d657dfa5f39bd4058
ali uzair <uzairnns5@gmail.com>
2021-06-10 13:01:09 +0500

    fragments

---
+++ Fragments/app/src/main/java/com/example/fragments/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    TabLayout tablayout;
+    TabItem tabItem1,tabItem2;
+    ViewPager viewPager;
+    PageAdapter pageAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tablayout=(TabLayout) findViewById(R.id.tablayout);
+        tabItem1=(TabItem)findViewById(R.id.tab1);
+        tabItem2=(TabItem)findViewById(R.id.tab2);
+        viewPager=(ViewPager) findViewById(R.id.viewPager);
+
+        pageAdapter=new PageAdapter(getSupportFragmentManager(),tablayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+
+        tablayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if(tab.getPosition()==0 || tab.getPosition()==1)
+                {
+                    pageAdapter.notifyDataSetChanged();
+                }
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tablayout));
+    }
+}
+
---
+++ Fragments/app/src/main/java/com/example/fragments/PageAdapter.java
@@ -0,0 +1,33 @@
+package com.example.fragments;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PageAdapter extends FragmentPagerAdapter {
+
+    int tabcount;
+
+    public PageAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        tabcount=behavior;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position)
+        {
+            case 0: return new ChatFragment();
+            case 1: return new TaskFragment();
+            default: return  null;
+
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabcount;
+    }
+}
